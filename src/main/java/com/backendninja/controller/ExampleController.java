baa19e176143a2a385036a19ea7e6393a4f4f8d8
unknown <rodrigo.ulloa@latam.com>
2020-07-29 22:07:26 -0400

    Video 04_07 se le pasa un objeto persona a la pagina cual va el controller a traves de Model y ModelAndView

--- src/main/java/com/backendninja/controller/ExampleController.java
+++ src/main/java/com/backendninja/controller/ExampleController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.backendninja.model.Person;
+
 @Controller
 @RequestMapping("/example") // la URL que va a tener este controlado
 public class ExampleController {
@@ -14,17 +16,17 @@ public class ExampleController {
 
 
 	// Tercera forma
-	@GetMapping("/exampleGetMapping") // desde la pagina nos tiene que venir el model, y le pasamos el parametro que esta en la pagina de example.html								 
+	@GetMapping("/exampleGetMapping") // desde la pagina nos tiene que venir el model, y le pasamos el objeto que esta en la pagina de example.html								 
 	public String exampleGetMapping(Model model) {
-		model.addAttribute("name", "exampleGetMapping");
+		model.addAttribute("person", new Person("exampleGetMapping",31));
 		return EXAMPLE_VIEW;
 	}
 
 	// Cuarta forma
 	@GetMapping("/exampleGetMappingModel") // en este ejemplo no nos entregan nada y lo hacemos desde aca
 	public ModelAndView exampleGetMappingModel() {
-		ModelAndView modelAndView=new ModelAndView(EXAMPLE_VIEW);//tenemos que pasarle el nombre de la plantilla para que reconozca
-		modelAndView.addObject("name", "exampleGetMappingModel");
+		ModelAndView modelAndView=new ModelAndView(EXAMPLE_VIEW);//tenemos que pasarle el objeto de la plantilla para que reconozca
+		modelAndView.addObject("person", new Person("exampleGetMappingModel",31));
 		return modelAndView;
 	}
 }
