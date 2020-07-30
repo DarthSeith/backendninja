8b99a85944aa776921d337f31b8d52760491b2d7
unknown <rodrigo.ulloa@latam.com>
2020-07-29 22:01:15 -0400

    Video 04_06 se le pasa parametros a la pagina cual va el controller a traves de Model y ModelAndView

--- src/main/java/com/backendninja/controller/ExampleController.java
+++ src/main/java/com/backendninja/controller/ExampleController.java
@@ -1,9 +1,9 @@
 package com.backendninja.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -12,29 +12,19 @@ public class ExampleController {
 
 	public static final String EXAMPLE_VIEW = "example";
 
-	// Primera Forma
-	@RequestMapping(value = "/exampleString", method = RequestMethod.GET) // el siguiente path y redirecciona a la
-																			// pagina que se encuentra en
-																			// /backendninja/src/main/resources/templates/helloworld.html
-																			// Las nuevas versiones utilizan la
-																			// annotation @GetMapping (La tercera forma)
-	public String exampleString() {
-		return EXAMPLE_VIEW;
-	}
-
-	// Segunda Forma
-	@RequestMapping(value = "/exampleMAV", method = RequestMethod.GET) // aca utilizamos ModelAndView Las nuevas
-																		// versiones utilizan la annotation
-																		// @GetMapping @GetMapping (La tercera forma)
-	public ModelAndView exampleMAV() {
-		return new ModelAndView(EXAMPLE_VIEW);
-	}
 
 	// Tercera forma
-	@GetMapping("/exampleGetMapping") // el siguiente path y redirecciona a la pagina que se encuentra en
-										// /backendninja/src/main/resources/templates/helloworld.html
-	public String exampleGetMapping() {
+	@GetMapping("/exampleGetMapping") // desde la pagina nos tiene que venir el model, y le pasamos el parametro que esta en la pagina de example.html								 
+	public String exampleGetMapping(Model model) {
+		model.addAttribute("name", "exampleGetMapping");
 		return EXAMPLE_VIEW;
 	}
 
+	// Cuarta forma
+	@GetMapping("/exampleGetMappingModel") // en este ejemplo no nos entregan nada y lo hacemos desde aca
+	public ModelAndView exampleGetMappingModel() {
+		ModelAndView modelAndView=new ModelAndView(EXAMPLE_VIEW);//tenemos que pasarle el nombre de la plantilla para que reconozca
+		modelAndView.addObject("name", "exampleGetMappingModel");
+		return modelAndView;
+	}
 }
