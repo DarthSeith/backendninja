c4fcb010c3890802f4af2d7f980b0208767360bb
unknown <rodrigo.ulloa@latam.com>
2020-07-29 22:18:44 -0400

    Video 04_08 se le pasa una lista de objetos personas a la pagina cual va el controller a traves de Model y ModelAndView

--- src/main/java/com/backendninja/controller/ExampleController.java
+++ src/main/java/com/backendninja/controller/ExampleController.java
@@ -1,5 +1,8 @@
 package com.backendninja.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,7 +21,8 @@ public class ExampleController {
 	// Tercera forma
 	@GetMapping("/exampleGetMapping") // desde la pagina nos tiene que venir el model, y le pasamos el objeto que esta en la pagina de example.html								 
 	public String exampleGetMapping(Model model) {
-		model.addAttribute("person", new Person("exampleGetMapping",31));
+		model.addAttribute("title", "Welcome exampleGetMapping");
+		model.addAttribute("people", getPeople());
 		return EXAMPLE_VIEW;
 	}
 
@@ -26,7 +30,19 @@ public class ExampleController {
 	@GetMapping("/exampleGetMappingModel") // en este ejemplo no nos entregan nada y lo hacemos desde aca
 	public ModelAndView exampleGetMappingModel() {
 		ModelAndView modelAndView=new ModelAndView(EXAMPLE_VIEW);//tenemos que pasarle el objeto de la plantilla para que reconozca
-		modelAndView.addObject("person", new Person("exampleGetMappingModel",31));
+		modelAndView.addObject("title", "Welcome exampleGetMappingModel");
+		modelAndView.addObject("people", getPeople());
 		return modelAndView;
 	}
-}
+	
+	private List<Person> getPeople(){
+		List<Person> lst=new ArrayList<Person>();
+		lst.add(new Person("one",1));
+		lst.add(new Person("two",2));
+		lst.add(new Person("tree",3));
+		lst.add(new Person("forth",4));
+		lst.add(new Person("five",5));
+		lst.add(new Person("six",6));
+		return lst;
+	}
+} 
